45b4753ba268a7a3e19e7e7ea431e7bc8a23eae4
Baolin Li <baolin.li1994@gmail.com>
2023-08-13 14:10:46 -0400

    demo framework working

--- experiments/detect.py
+++ experiments/detect.py
@@ -16,24 +16,25 @@ targets_path = 'utils/targets_c60.mat'
 parser = argparse.ArgumentParser()
 # Get data configuration
 if platform == 'darwin':  # macos
-    parser.add_argument('-image_folder', type=str, default='/Users/glennjocher/Downloads/DATA/xview/train_images/5.tif')
-    parser.add_argument('-output_folder', type=str, default='./output_xview', help='path to outputs')
+    parser.add_argument('--image_folder', type=str, default='/Users/glennjocher/Downloads/DATA/xview/train_images/5.tif')
+    parser.add_argument('--output_folder', type=str, default='./output_xview', help='path to outputs')
     cuda = True  # torch.cuda.is_available()
 else:  # gcp
     # cd yolo && python3 detect.py -secondary_classifier 1
-    parser.add_argument('-image_folder', type=str, default='/home/ubuntu/GIT/xview_dataset/val_images', help='path to images')
-    parser.add_argument('-output_folder', type=str, default='../output', help='path to outputs')
+    parser.add_argument('--image_folder', type=str, default='/work/li.baol/val_images', help='path to images')
+    parser.add_argument('--output_folder', type=str, default='../output', help='path to outputs')
     cuda = True
 
-parser.add_argument('-plot_flag', type=bool, default=False)
-parser.add_argument('-secondary_classifier', type=bool, default=False)
-parser.add_argument('-cfg', type=str, default='cfg/c60_a30symmetric.cfg', help='cfg file path')
-parser.add_argument('-class_path', type=str, default='data/xview.names', help='path to class label file')
-parser.add_argument('-conf_thres', type=float, default=0.99, help='object confidence threshold')
-parser.add_argument('-nms_thres', type=float, default=0.4, help='iou threshold for non-maximum suppression')
-parser.add_argument('-batch_size', type=int, default=2, help='size of the batches')
-parser.add_argument('-img_size', type=int, default=32 * 51, help='size of each image dimension')
-parser.add_argument('-tc', type=str, default='none', help='testcase of hardware')
+parser.add_argument('--plot_flag', type=bool, default=False)
+parser.add_argument('--secondary_classifier', type=bool, default=False)
+parser.add_argument('--cfg', type=str, default='cfg/c60_a30symmetric.cfg', help='cfg file path')
+parser.add_argument('--class_path', type=str, default='data/xview.names', help='path to class label file')
+parser.add_argument('--conf_thres', type=float, default=0.99, help='object confidence threshold')
+parser.add_argument('--nms_thres', type=float, default=0.4, help='iou threshold for non-maximum suppression')
+parser.add_argument('--batch_size', type=int, default=2, help='size of the batches')
+parser.add_argument('--img_size', type=int, default=32 * 51, help='size of each image dimension')
+parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
+parser.add_argument('--tc', type=str, default='none', help='testcase of hardware')
 
 opt = parser.parse_args()
 print(opt)
@@ -55,17 +56,6 @@ def detect(opt):
     model.to(device).eval()
     del checkpoint
 
-    # current = model.state_dict()
-    # saved = checkpoint['model']
-    # # 1. filter out unnecessary keys
-    # saved = {k: v for k, v in saved.items() if ((k in current) and (current[k].shape == v.shape))}
-    # # 2. overwrite entries in the existing state dict
-    # current.update(saved)
-    # # 3. load the new state dict
-    # model.load_state_dict(current)
-    # model.to(device).eval()
-    # del checkpoint, current, saved
-
     # Load model 2
     if opt.secondary_classifier:
         model2 = ConvNetb()
@@ -74,17 +64,6 @@ def detect(opt):
         model2.load_state_dict(checkpoint['model'])
         model2.to(device).eval()
         del checkpoint
-
-        # current = model2.state_dict()
-        # saved = checkpoint['model']
-        # # 1. filter out unnecessary keys
-        # saved = {k: v for k, v in saved.items() if ((k in current) and (current[k].shape == v.shape))}
-        # # 2. overwrite entries in the existing state dict
-        # current.update(saved)
-        # # 3. load the new state dict
-        # model2.load_state_dict(current)
-        # model2.to(device).eval()
-        # del checkpoint, current, saved
     else:
         model2 = None
 
@@ -92,83 +71,47 @@ def detect(opt):
     classes = load_classes(opt.class_path)  # Extracts class labels from file
     dataloader = ImageFolder(opt.image_folder, batch_size=opt.batch_size, img_size=opt.img_size)
 
-    imgs = []  # Stores image paths
-    img_detections = []  # Stores detections for each image index
-    prev_time = time.time()
-    detections = None
-    mat_priors = scipy.io.loadmat(targets_path)
+    # prev_time = time.time()
+    # detections = None
+    # mat_priors = scipy.io.loadmat(targets_path)
     lat_list = []
+    preds = []
 
-    for batch_i, (img_paths, img) in enumerate(dataloader):
-        print(batch_i, img.shape, end=' ')
+    for epoch in range(opt.epochs):
+        for batch_i, (img_paths, img) in enumerate(dataloader):
+            print(batch_i, img.shape, end=' ')
 
-        img_ud = np.ascontiguousarray(np.flip(img, axis=1))
-        img_lr = np.ascontiguousarray(np.flip(img, axis=2))
+            length = opt.img_size
+            ni = int(math.ceil(img.shape[1] / length))  # up-down
+            nj = int(math.ceil(img.shape[2] / length))  # left-right
+            t_start = time.time()
+            for i in range(ni):  # for i in range(ni - 1):
+    #            print('row %g/%g: ' % (i, ni), end='')
 
-        preds = []
-        length = opt.img_size
-        ni = int(math.ceil(img.shape[1] / length))  # up-down
-        nj = int(math.ceil(img.shape[2] / length))  # left-right
-        t_start = time.time()
-        for i in range(ni):  # for i in range(ni - 1):
-#            print('row %g/%g: ' % (i, ni), end='')
+                for j in range(nj):  # for j in range(nj if i==0 else nj - 1):
+    #                print('%g ' % j, end='', flush=True)
 
-            for j in range(nj):  # for j in range(nj if i==0 else nj - 1):
-#                print('%g ' % j, end='', flush=True)
+                    # forward scan
+                    y2 = min((i + 1) * length, img.shape[1])
+                    y1 = y2 - length
+                    x2 = min((j + 1) * length, img.shape[2])
+                    x1 = x2 - length
 
-                # forward scan
-                y2 = min((i + 1) * length, img.shape[1])
-                y1 = y2 - length
-                x2 = min((j + 1) * length, img.shape[2])
-                x1 = x2 - length
+                    # Get detections
+                    with torch.no_grad():
+                        # Normal orientation
+                        chip = torch.from_numpy(img[:, y1:y2, x1:x2]).unsqueeze(0).to(device)
+                        pred = model(chip)
+                        pred = pred[pred[:, :, 4] > opt.conf_thres]
+                        if len(pred) > 0:
+                            pred[:, 0] += x1
+                            pred[:, 1] += y1
+                            preds.append(pred.unsqueeze(0))
 
-                # Get detections
-                with torch.no_grad():
-                    # Normal orientation
-                    chip = torch.from_numpy(img[:, y1:y2, x1:x2]).unsqueeze(0).to(device)
-                    pred = model(chip)
-                    pred = pred[pred[:, :, 4] > opt.conf_thres]
-                    # if (j > 0) & (len(pred) > 0):
-                    #     pred = pred[(pred[:, 0] - pred[:, 2] / 2 > 2)]  # near left border
-                    # if (j < nj) & (len(pred) > 0):
-                    #     pred = pred[(pred[:, 0] + pred[:, 2] / 2 < 606)]  # near right border
-                    # if (i > 0) & (len(pred) > 0):
-                    #     pred = pred[(pred[:, 1] - pred[:, 3] / 2 > 2)]  # near top border
-                    # if (i < ni) & (len(pred) > 0):
-                    #     pred = pred[(pred[:, 1] + pred[:, 3] / 2 < 606)]  # near bottom border
-                    if len(pred) > 0:
-                        pred[:, 0] += x1
-                        pred[:, 1] += y1
-                        preds.append(pred.unsqueeze(0))
-
-                    # # Flipped Up-Down
-                    # chip = torch.from_numpy(img_ud[:, y1:y2, x1:x2]).unsqueeze(0).to(device)
-                    # pred = model(chip)
-                    # pred = pred[pred[:, :, 4] > opt.conf_thres]
-                    # if len(pred) > 0:
-                    #     pred[:, 0] += x1
-                    #     pred[:, 1] = img.shape[1] - (pred[:, 1] + y1)
-                    #     preds.append(pred.unsqueeze(0))
-
-                    # # Flipped Left-Right
-                    # chip = torch.from_numpy(img_lr[:, y1:y2, x1:x2]).unsqueeze(0).to(device)
-                    # pred = model(chip)
-                    # pred = pred[pred[:, :, 4] > opt.conf_thres]
-                    # if len(pred) > 0:
-                    #     pred[:, 0] = img.shape[2] - (pred[:, 0] + x1)
-                    #     pred[:, 1] += y1
-                    #     preds.append(pred.unsqueeze(0))
-
-        if len(preds) > 0:
-            detections = non_max_suppression(torch.cat(preds, 1), opt.conf_thres, opt.nms_thres, mat_priors, img,
-                                             model2, device)
-            img_detections.extend(detections)
-            imgs.extend(img_paths)
-
-#        print('Batch %d... (Done %.3fs)' % (batch_i, time.time() - prev_time))
-        t_end = time.time()
-        lat_list.append(round((t_end - t_start)*1000,3)) # in ms
-        print(f'item {batch_i} done')
+    #        print('Batch %d... (Done %.3fs)' % (batch_i, time.time() - prev_time))
+            t_end = time.time()
+            lat_list.append(round((t_end - t_start)*1000,3)) # in ms
+            print(f'item {batch_i} done')
 
     with open(f'experiments/logs/lat_list_{opt.tc}.json', 'w') as f:
         json.dump(lat_list, f, indent=4)
@@ -176,68 +119,7 @@ def detect(opt):
     # Bounding-box colors
     color_list = [[random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)] for _ in range(len(classes))]
 
-    if len(img_detections) == 0:
-        return
-
-#    # Iterate through images and save plot of detections
-#    for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
-#        print("image %g: '%s'" % (img_i, path))
-#
-#        if opt.plot_flag:
-#            img = cv2.imread(path)
-#
-#        # # The amount of padding that was added
-#        # pad_x = max(img.shape[0] - img.shape[1], 0) * (opt.img_size / max(img.shape))
-#        # pad_y = max(img.shape[1] - img.shape[0], 0) * (opt.img_size / max(img.shape))
-#        # # Image height and width after padding is removed
-#        # unpad_h = opt.img_size - pad_y
-#        # unpad_w = opt.img_size - pad_x
-#
-#        # Draw bounding boxes and labels of detections
-#        if detections is not None:
-#            unique_classes = detections[:, -1].cpu().unique()
-#            bbox_colors = random.sample(color_list, len(unique_classes))
-#
-#            # write results to .txt file
-#            results_path = os.path.join(opt.output_folder, path.split('/')[-1])
-#            if os.path.isfile(results_path + '.txt'):
-#                os.remove(results_path + '.txt')
-#
-#            results_img_path = os.path.join(opt.output_folder + '_img', path.split('/')[-1])
-#            with open(results_path.replace('.bmp', '.tif') + '.txt', 'a') as file:
-#                for i in unique_classes:
-#                    n = (detections[:, -1].cpu() == i).sum()
-#                    print('%g %ss' % (n, classes[int(i)]))
-#
-#                for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
-#                    # Rescale coordinates to original dimensions
-#                    # box_h = ((y2 - y1) / unpad_h) * img.shape[0]
-#                    # box_w = ((x2 - x1) / unpad_w) * img.shape[1]
-#                    # y1 = (((y1 - pad_y // 2) / unpad_h) * img.shape[0]).round().item()
-#                    # x1 = (((x1 - pad_x // 2) / unpad_w) * img.shape[1]).round().item()
-#                    # x2 = (x1 + box_w).round().item()
-#                    # y2 = (y1 + box_h).round().item()
-#                    x1, y1, x2, y2 = max(x1, 0), max(y1, 0), max(x2, 0), max(y2, 0)
-#
-#                    # write to file
-#                    xvc = xview_indices2classes(int(cls_pred))  # xview class
-#                    # if (xvc != 21) & (xvc != 72):
-#                    file.write(('%g %g %g %g %g %g \n') % (x1, y1, x2, y2, xvc, cls_conf * conf))
-#
-#                    if opt.plot_flag:
-#                        # Add the bbox to the plot
-#                        label = '%s %.2f' % (classes[int(cls_pred)], cls_conf) if cls_conf > 0.05 else None
-#                        color = bbox_colors[int(np.where(unique_classes == int(cls_pred))[0])]
-#                        plot_one_box([x1, y1, x2, y2], img, label=label, color=color, line_thickness=1)
-#
-#            if opt.plot_flag:
-#                # Save generated image with detections
-#                cv2.imwrite(results_img_path.replace('.bmp', '.jpg').replace('.tif', '.jpg'), img)
-
-    if opt.plot_flag:
-        from scoring import score
-        score.score(opt.output_folder + '/', '/Users/glennjocher/Downloads/DATA/xview/xView_train.geojson', '.')
-
+    return
 
 class ConvNetb(nn.Module):
     def __init__(self, num_classes=60):
---
+++ experiments/launch_parallel_detections.py
@@ -0,0 +1,68 @@
+from mig import mig_helper
+import json
+from pathlib import Path
+import subprocess
+import argparse
+import socket
+import time
+from typing import List
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+MIG_DIR = f'{str(ROOT)}/mig'
+
+def config_gpu(gpuid, mig_config: str):
+    # reset the GPU
+    mig_helper.reset_mig(gpuid)
+
+    # partition to desired config
+    with open(f'{MIG_DIR}/partition_code.json') as f:
+        partition = json.load(f)
+    mig_helper.do_partition(gpuid, partition[mig_config])
+
+def launch(mig_config: str):
+    with open(f'{MIG_DIR}/partition_code.json') as f:
+        partition = json.load(f)
+    num_slices = len(partition[mig_config])
+    with open(f'{MIG_DIR}/mig_device_autogen.json') as f:
+        device_json = json.load(f)
+
+    Path('/scratch/li.baol/xview_logs').mkdir(parents=True, exist_ok=True)
+    Path(f'experiments/logs/lat_list_{mig_config}/').mkdir(parents=True, exist_ok=True)
+    mig_list = device_json[hostname][f'gpu0'][mig_config]
+    procs = []
+    for i in range(num_slices):
+        device = mig_list[i]
+        cmd = f'CUDA_VISIBLE_DEVICES={device} python detect.py \
+                --epochs {args.epochs} --batch_size {args.batch_size} \
+                --tc {mig_config}/mig_{i}' 
+        print(cmd)
+        out_file = f'/scratch/li.baol/xview_logs/yolo{i}.out'
+        err_file = f'/scratch/li.baol/xview_logs/yolo{i}.err'
+        with open(out_file, 'w+') as out, open(err_file, 'w+') as err:
+            proc = subprocess.Popen([cmd], shell=True, stdout=out, stderr=err)
+            procs.append(proc)
+        time.sleep(0.1)
+    for proc in procs:
+        proc.wait()
+
+def parse():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--mig_config', type=str, default='0', help='configuration of MIG according to NVIDIA table, 0-17')    
+    # parser.add_argument('--gpuid', type=int, default=0, help='GPU ID (0 or 1)')  
+    # parser.add_argument('--weights', type=str, default='yolov5x6', help='model name: yolov5s, yolov5x, yolov5x6')
+    # parser.add_argument('--term', action='store_true', help='terminate services', default=False)        
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
+    parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
+    parser.add_argument('--mig_config', type=str, default="0", help='testcase')
+    args = parser.parse_args()
+    # args.imgsz *= 2 if len(args.imgsz) == 1 else 1  # expand
+    print(args)
+    return args    
+
+if __name__ == '__main__':
+    args = parse()
+    hostname = socket.gethostname()
+    config_gpu(0, args.mig_config)
+    time.sleep(5)
+    launch(args.mig_config)
+
